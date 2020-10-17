45cd3253536f70c818eadce9fd639da1200861b5
Ebenezer Justice <justiceebenezer4@gmail.com>
2020-10-17 15:08:13 +0100

    initial commit

--- app/src/main/java/com/mobileedu8/andelaschools/Activity/LoginScreen.java
+++ app/src/main/java/com/mobileedu8/andelaschools/Activity/LoginScreen.java
@@ -2,15 +2,46 @@ package com.mobileedu8.andelaschools.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.mobileedu8.andelaschools.R;
 
 public class LoginScreen extends AppCompatActivity {
 
+    //UI's
+    private Button login;
+    private Button register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        initID();
+
+        login.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent loginIntent = new Intent(LoginScreen.this, LoginActivity.class);
+                 startActivity(loginIntent);
+             }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginScreen.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
     }
+
+    //finds relevant id's for initialization
+    private void initID() {
+        login = findViewById(R.id.button_login);
+        register = findViewById(R.id.button_register);
+    }
+
 }
