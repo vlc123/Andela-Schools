79575d5691315bf5af339bc4a84ddbaf7070a2ef
Ebenezer Justice <justiceebenezer4@gmail.com>
2020-10-17 13:16:54 +0100

    initial commit

---
+++ app/src/main/java/com/mobileedu8/andelaschools/Activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.mobileedu8.andelaschools.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mobileedu8.andelaschools.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
