4cd65f7d1d601d1af6658865bc0a8ff5f9129532
vlc123 <cedricnampalegiga10@gmail.com>
2020-10-19 16:24:52 +0100

    UI Design update

--- app/src/main/java/com/mobileedu8/andelaschools/Activity/RegisterActivity.java
+++ app/src/main/java/com/mobileedu8/andelaschools/Activity/RegisterActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.tabs.TabLayout;
 import com.mobileedu8.andelaschools.Adapters.RegisterAdapter;
@@ -13,9 +16,8 @@ public class RegisterActivity extends AppCompatActivity {
 
     private RegisterAdapter adapter;
     private TabLayout tabLayout;
-    private androidx.appcompat.widget.Toolbar toolbar;
     private ViewPager viewPager;
-
+    private ImageView registerBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +25,26 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         initID();
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
+        initAction();
+    }
+
+    private void initAction() {
+        registerBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     private void initID() {
+        registerBack = (ImageView) findViewById(R.id.registerBack);
         adapter = new RegisterAdapter(getSupportFragmentManager());
         viewPager = findViewById(R.id.viewpager);
         tabLayout = findViewById(R.id.tablayout);
-        toolbar = findViewById(R.id.toolbar);
+
+        //setup viewpager and TabLayout
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
